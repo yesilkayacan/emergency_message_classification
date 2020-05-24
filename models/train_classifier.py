3251da45b10438e7423851eda8b23e7a79be0198
yesilkayacan@gmail.com <yesilkayacan@gmail.com>
2020-05-24 19:39:28 +0300

    feat: train_classifier.py migration
    Migration from ipynb to python script has started for classifier model. All functions have been moved but not tested or documented.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,93 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import pickle
+import numpy as np
 
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report, accuracy_score, precision_score, recall_score
+
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('DisasterMessageTable', engine)
+    X = df['message'].values
+    Y = df[df.columns[4:]].values
+    category_names = df.columns[4:]
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    ''' '''
+    stop_words = stopwords.words("english")
+    
+    text = re.sub(r"[a-zA-Z0-9]", " ", text.lower())
+    
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    
+    clean_tokens = [lemmatizer.lemmatize(tok).strip() for tok in tokens if tok not in stop_words]
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(KNeighborsClassifier()))
+        ])
+    
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'clf__estimator__n_neighbors': [2, 4, 6]
+        }
+    
+    cv = GridSearchCV(pipeline, param_grid=parameters, n_jobs=1, verbose=2)
+    
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    
+    y_pred = model.predict(X_test)
+    
+    accuracy_list = []
+    precision_list = []
+    recall_list = []
+    for i in range(len(category_names)):
+        accuracy_list.append(accuracy_score(Y_test[:,i], y_pred[:,i]))
+        precision_list.append(precision_score(Y_test[:,i], y_pred[:,i], average='weighted'))
+        recall_list.append(recall_score(Y_test[:,i], y_pred[:,i], average='weighted'))
+        
+    print('Model performance:\naccuracy: {}\nprecisicion: {}\nrecall: {}'
+          .format(model_performance(y_true, y_predicted)))
+    return np.mean(accuracy_list), np.mean(precision_list), np.mean(recall_list)
 
 
 def save_model(model, model_filepath):
-    pass
+    
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 
 def main():

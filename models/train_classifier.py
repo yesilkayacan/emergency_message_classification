e9d7b2a5cdf85d2ef4ebcea0cf903133ba73039b
CY <yesilkayacan@gmail.com>
2020-05-25 20:11:03 +0300

    feat:test train split with stratify
    Test train split now considers the first target vector as stratify target to get a better training data set.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -192,7 +192,7 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y[:,0])
         
         print('Building model...')
         model = build_model()
